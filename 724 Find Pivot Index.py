51dd7f1c194da3e4f8582e49b07f94ce46c9dcfc
Spencer-Breding <55817041+Spencer-Breding@users.noreply.github.com>
2023-03-04 13:44:04 -0800

    Update 724 Find Pivot Index.py

--- 724 Find Pivot Index.py
+++ 724 Find Pivot Index.py
@@ -1,5 +1,3 @@
-# Runtime: 28ms (Beat 99.52% of submissions)
-# Memory: 14.1 MB (Beat 19.18% of submissions)
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         sum = 0
