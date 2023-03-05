afbef7708fd59fbb542c33a72cab9906ec4640b7
Spencer-Breding <55817041+Spencer-Breding@users.noreply.github.com>
2023-03-04 22:40:44 -0800

    Create 724 Find Pivot Index.py

---
+++ 724 Find Pivot Index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        leftSum=0
+        rightSum=sum(nums)
+        print(leftSum)
+        print(rightSum)
+        for i in range(len(nums)):
+            rightSum -= nums[i]
+            leftSum += nums[i]
+            if (leftSum-nums[i]) == rightSum:
+                return i
+        return -1
