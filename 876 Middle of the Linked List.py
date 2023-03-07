68e962cde2e59e32eda187e0949cbf4f332ec59d
Spencer-Breding <55817041+Spencer-Breding@users.noreply.github.com>
2023-03-07 08:27:32 -0800

    Create 876 Middle of the Linked List.py

---
+++ 876 Middle of the Linked List.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        count = 0
+        clone = head
+        while clone:
+            count += 1
+            clone = clone.next
+        if count%2==0:
+            count+=1
+        count = -(count // -2)
+        while count>1:
+            head = head.next
+            count -= 1
+        return head
