f861f8ba9d95905549db23b65ae382b014c65aa1
Spencer-Breding <55817041+Spencer-Breding@users.noreply.github.com>
2023-03-09 14:30:16 -0800

    Update 876 Middle of the Linked List.py

--- 876 Middle of the Linked List.py
+++ 876 Middle of the Linked List.py
@@ -17,3 +17,19 @@ class Solution:
             head = head.next
             count -= 1
         return head
+
+    
+# Another Solution
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        count = 0
+        fast, slow = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
